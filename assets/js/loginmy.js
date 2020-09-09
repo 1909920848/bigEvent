1b6e05c80e5d717196914991aaebc71c641ade3b
1909920848 <1909920848@qq.com>
2020-09-09 15:47:54 +0800

    登录注册布局已完成

---
+++ assets/js/loginmy.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('#link_reg').on('click', function() {
+        $(".login").hide()
+        $(".register").show()
+    })
+    $('#link-login').on('click', function() {
+        $(".login").show()
+        $(".register").hide()
+    })
+
+})
